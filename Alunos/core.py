80417282a7239496012fc2ac8c97f78d6235b5dd
Leonardo Nascimento <leonardofernandespro@gmail.com>
2025-07-23 13:09:22 -0300

    Realizado a adição das funções do programa

--- Alunos/core.py
+++ Alunos/core.py
@@ -3,8 +3,16 @@
 alunos = []
 
 def cadastrar_aluno():
-    nome = input("Digite o nome do aluno: ")
+    nome = input("\nDigite o nome do aluno: ")
     matricula = int(input("Digite a matrícula do aluno: "))
+
+    for aluno in alunos:
+        valor = matricula
+        if matricula == aluno['matricula']:
+            while matricula == aluno['matricula']:
+                print("\nMatrícula já em uso, informe uma matrícula válida.")
+                matricula = int(input("Digite a matrícula do aluno: "))
+
     idade = int(input("Digite a idade do aluno: "))
     lista_de_notas = []
 
@@ -12,7 +20,8 @@ def cadastrar_aluno():
         "nome": nome,
         "matricula": matricula,
         "idade": idade,
-        "notas": lista_de_notas
+        "notas": lista_de_notas,
+        "status": False
     }
 
     alunos.append(novo_aluno)
@@ -27,25 +36,48 @@ def adicionar_nota():
                     aluno['notas'].append(nova_nota)
                     continuar = input("Você deseja inserir mais alguma nota? (S/N)").strip().upper()
                     if continuar in "N":
-                        break
-                    elif continuar in "S":
-                        print(f"\nAdicionando nota ao aluno...")
+                        return
             else:
                 print(f"Valor incorreto tente novamente.")
+                return
 
 
 def listar_alunos():
     print(f"\n\n{'=' * 30}\nLista de alunos \n{'=' * 30}")
     for aluno in alunos:
-        print(f"Nome do aluno: {aluno['nome']} \nMatrícula do aluno: {aluno['matricula']} \nIdade do aluno: {aluno['idade']} \nNotas do aluno: {aluno['notas']}")
+        print(f"Nome do aluno: {aluno['nome']} \nMatrícula do aluno: {aluno['matricula']} \nIdade do aluno: {aluno['idade']} \nNotas do aluno: {aluno['notas']} \n{'-' * 15}")
 
 
+def atualizar_aluno():
+    global alunos
+    matricula = int(input("Informe a matrícula do aluno: "))
+    for aluno in alunos:
+        if matricula == aluno['matricula']:
+            novo_nome = input("\nDigite o novo nome do aluno: ")
+            novo_matricula = int(input("Digite a nova matrícula do aluno: "))
+            novo_idade = int(input("Digite a nova idade do aluno: "))
+
+            aluno['nome'] = novo_nome
+            aluno['matricula'] = novo_matricula
+            aluno['idade'] = novo_idade
+
+
+def deletar_aluno():
+    global alunos
+
+    matricula = int(input("Informe a matrícula do aluno a ser excluída: "))
+    
+    for aluno in alunos:
+        if matricula == aluno['matricula']:
+            aluno['status'] = True
+
+    alunos = [aluno for aluno in alunos if not aluno['status']]
 
 
 
 def iniciar():
     while True:
-        inicio = int(input("\n\nEscolha uma opção: \n[ 1 ] - Cadastra aluno \n[ 2 ] - Adicionar nota \n[ 3 ] - Listar alunos \n[ 8 ] - Sair e salvar dados\n"))
+        inicio = int(input("\n\nEscolha uma opção: \n[ 1 ] - Cadastra aluno \n[ 2 ] - Adicionar nota \n[ 3 ] - Listar alunos \n[ 4 ] - Atualizar dados do aluno \n[ 5 ] - Deletar aluno \n[ 6 ] - Sair e salvar dados\n"))
 
         if inicio == 1:
             cadastrar_aluno()
@@ -53,7 +85,11 @@ def iniciar():
             adicionar_nota()
         elif inicio == 3:
             listar_alunos()
-        elif inicio == 8:
+        elif inicio == 4:
+            atualizar_aluno()
+        elif inicio == 5:
+            deletar_aluno()
+        elif inicio == 6:
             print("Saindo...")
 
 
