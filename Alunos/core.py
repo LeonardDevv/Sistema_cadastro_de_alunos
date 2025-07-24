845f942fc0d5728088a96893594d44a35f228c40
Leonardo Nascimento <leonardofernandespro@gmail.com>
2025-07-24 14:50:46 -0300

    Realizado a implmentação das funções do programa

--- Alunos/core.py
+++ Alunos/core.py
@@ -1,21 +1,24 @@
-# Funções do sistema
+import json
+import os
 
-alunos = []
+if os.path.exists('alunos.json'):
+    try:
+        with open('alunos.json', 'r', encoding='utf8') as f:
+            alunos = json.load(f)
+    except Exception as e:
+        print(f"Erro ao carregar os dados do arquivo alunos.json: {e}")
+
 
 def cadastrar_aluno():
     nome = input("\nDigite o nome do aluno: ")
     matricula = int(input("Digite a matrícula do aluno: "))
-
     for aluno in alunos:
-        valor = matricula
         if matricula == aluno['matricula']:
             while matricula == aluno['matricula']:
                 print("\nMatrícula já em uso, informe uma matrícula válida.")
                 matricula = int(input("Digite a matrícula do aluno: "))
-
     idade = int(input("Digite a idade do aluno: "))
     lista_de_notas = []
-
     novo_aluno = {
         "nome": nome,
         "matricula": matricula,
@@ -23,12 +26,19 @@ def cadastrar_aluno():
         "notas": lista_de_notas,
         "status": False
     }
-
     alunos.append(novo_aluno)
 
+
 def adicionar_nota():
+    matricula = int(input("Digite a matrícula do aluno: "))
+    id = int()
+    for aluno in alunos:
+            if matricula == aluno['matricula']:
+                id = matricula
+    if id != matricula:
+                print("Matrícula não encontrada, tente novamente.")
+                adicionar_nota()
     while True:
-        id = int(input("Digite a matrícula do aluno: "))
         for aluno in alunos:
             if id == aluno['matricula']:
                 while True:
@@ -37,48 +47,58 @@ def adicionar_nota():
                     continuar = input("Você deseja inserir mais alguma nota? (S/N)").strip().upper()
                     if continuar in "N":
                         return
-            else:
-                print(f"Valor incorreto tente novamente.")
-                return
 
 
 def listar_alunos():
     print(f"\n\n{'=' * 30}\nLista de alunos \n{'=' * 30}")
     for aluno in alunos:
-        print(f"Nome do aluno: {aluno['nome']} \nMatrícula do aluno: {aluno['matricula']} \nIdade do aluno: {aluno['idade']} \nNotas do aluno: {aluno['notas']} \n{'-' * 15}")
+        print(f"Nome do aluno: {aluno['nome']} \nMatrícula do aluno: {aluno['matricula']} \nIdade do aluno: {aluno['idade']} \nNotas do aluno: {aluno['notas']} \n{'-' * 30}")
 
 
 def atualizar_aluno():
     global alunos
     matricula = int(input("Informe a matrícula do aluno: "))
+    id = int()
     for aluno in alunos:
-        if matricula == aluno['matricula']:
-            novo_nome = input("\nDigite o novo nome do aluno: ")
-            novo_matricula = int(input("Digite a nova matrícula do aluno: "))
-            novo_idade = int(input("Digite a nova idade do aluno: "))
-
-            aluno['nome'] = novo_nome
-            aluno['matricula'] = novo_matricula
-            aluno['idade'] = novo_idade
+            if matricula == aluno['matricula']:
+                id = matricula
+    if id != matricula:
+                print("Matrícula não encontrada, tente novamente.")
+                atualizar_aluno()
+    while True:
+        for aluno in alunos:
+            if matricula == aluno['matricula']:
+                novo_nome = input("\nDigite o novo nome do aluno: ")
+                novo_matricula = int(input("Digite a nova matrícula do aluno: "))
+                novo_idade = int(input("Digite a nova idade do aluno: "))
+                aluno['nome'] = novo_nome
+                aluno['matricula'] = novo_matricula
+                aluno['idade'] = novo_idade
+                return
 
 
 def deletar_aluno():
     global alunos
-
     matricula = int(input("Informe a matrícula do aluno a ser excluída: "))
-    
+    id = int()
     for aluno in alunos:
-        if matricula == aluno['matricula']:
-            aluno['status'] = True
-
-    alunos = [aluno for aluno in alunos if not aluno['status']]
-
+            if matricula == aluno['matricula']:
+                id = matricula
+    if id != matricula:
+                print("Matrícula não encontrada, tente novamente.")
+                deletar_aluno()
+    while True:
+        for aluno in alunos:
+            if matricula == aluno['matricula']:
+                print(f"\nAluno(a) {aluno['nome']} deletado.")
+                aluno['status'] = True     
+                alunos = [aluno for aluno in alunos if not aluno['status']]
+                return
 
 
 def iniciar():
     while True:
         inicio = int(input("\n\nEscolha uma opção: \n[ 1 ] - Cadastra aluno \n[ 2 ] - Adicionar nota \n[ 3 ] - Listar alunos \n[ 4 ] - Atualizar dados do aluno \n[ 5 ] - Deletar aluno \n[ 6 ] - Sair e salvar dados\n"))
-
         if inicio == 1:
             cadastrar_aluno()
         elif inicio == 2:
@@ -90,6 +110,13 @@ def iniciar():
         elif inicio == 5:
             deletar_aluno()
         elif inicio == 6:
-            print("Saindo...")
+            print("Salvando dados e saindo...")
+            try:
+                with open('alunos.json', 'w', encoding='utf8') as f:
+                    json.dump(alunos, f, ensure_ascii=False, indent=4)
+                    print("Dados salvos com sucesso em alunos.json")
+                    return
+            except Exception as e:
+                print(f"Ocorreu um erro ao salvar os dados: {e}")
 
-
+    
